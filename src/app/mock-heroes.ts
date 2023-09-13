927635ce32a11987ece5beb68e3e494f6cb11e25
carolrosatto <carolrosatto@gmail.com>
2023-09-13 20:34:03 -0300

    Criando mock

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,28 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Wolverine',
+  },
+  {
+    id: 2,
+    name: 'Homem Aranha',
+  },
+  {
+    id: 3,
+    name: 'Capitã Marvel',
+  },
+  {
+    id: 4,
+    name: 'Mulher Maravilha',
+  },
+  {
+    id: 5,
+    name: 'Batman',
+  },
+  {
+    id: 6,
+    name: 'Aquaman',
+  },
+];
