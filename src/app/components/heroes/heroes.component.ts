18ef8317d6a63cbf4c925364b368565bdf35723b
carolrosatto <carolrosatto@gmail.com>
2023-09-20 15:23:11 -0300

    Ajustes do hero component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero.model';
 import { HeroService } from 'src/app/services/hero/hero.service';
-import { MessageService } from 'src/app/services/message/message.service';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name'];
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService, 
-    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -23,10 +20,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
   }
 }
